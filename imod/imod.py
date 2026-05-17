9166f859c2deebbdb923e1db4014ea33798d659b
David Samah <lookforsamah@gmail.com>
2026-05-17 12:22:35 +0300

    Linear Reg + Sine + mx + c cummulative

---
+++ imod/imod.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 10, 100)
+
+
+
+m = 2
+c = 1
+y_linear = m * x + c
+
+
+y_sine = np.sin(x)
+
+noise = np.random.normal(0, 0.5, size=len(x))
+y_noisy = 2 * x + 1 + noise
+
+
+plt.figure(figsize=(10, 6))
+plt.plot(x, y_linear, label = "Linear: y = mx + c", linewidth=2)
+plt.plot(x, y_sine, label="Sine wave: sin(x)", linewidth = 2.0)
+plt.scatter(x, y_noisy, label= "Noisy data(stats)", alpha = 0.5, s = 20)
+
+plt.legend()
+plt.title("Linear vs Sine vs Statistical data")
+plt.grid(True)
+
+plt.show()
