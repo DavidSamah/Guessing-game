02c24ce59bc97caf107debec7bd622fee11fa9b6
David Samah <lookforsamah@gmail.com>
2026-05-18 01:01:31 +0300

    Successful

--- imod/imod.py
+++ imod/imod.py
@@ -11,7 +11,8 @@ y_linear = m * x + c
 
 
 y_sine = np.sin(x)
-
+y_cosine = np.cos(x)
+y_tan = np.tan(x)
 noise = np.random.normal(0, 0.5, size=len(x))
 y_noisy = 2 * x + 1 + noise
 
@@ -19,6 +20,7 @@ y_noisy = 2 * x + 1 + noise
 plt.figure(figsize=(10, 6))
 plt.plot(x, y_linear, label = "Linear: y = mx + c", linewidth=2)
 plt.plot(x, y_sine, label="Sine wave: sin(x)", linewidth = 2.0)
+
 plt.scatter(x, y_noisy, label= "Noisy data(stats)", alpha = 0.5, s = 20)
 
 plt.legend()
